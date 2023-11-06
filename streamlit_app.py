f23dec6f012ade1c6e45dee2aa8febad93118715
Zhenzhen <zhenzhen1108@hotmail.com>
2023-11-06 18:16:32 +1100

    TEST

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ from langchain.llms import QianfanLLMEndpoint
 from langchain.chains import LLMMathChain
 import streamlit.components.v1 as components
 import sys
-import qianfan
-import re
+
 
 from langchain.document_loaders import PyPDFLoader
 __import__('pysqlite3')
@@ -21,98 +20,6 @@ llm = QianfanLLMEndpoint(
     model="ERNIE-Bot-turbo",
     endpoint="eb-instant",
     )
-
-
-def eb_call(prompt, round):
-    print(prompt)
-    print('-' * 20,' EB Output ', '-'*20,"\n")
-
-    response = chat_comp.do(
-            model="ERNIE-Bot", 
-            messages=[{
-                "role": "user",
-                "content": prompt
-                }],
-            temperature=0.000000001,
-            functions=[
-                {
-                    "name": "delivery_inquiry",
-                    "description": "查询商品",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "location": {
-                                "type": "string",
-                                "description": "地址信息，包括街道、门牌号、城市、省份等信息"
-                                },
-                            "expect_price": {
-                                "type": "int",
-                                "description": "期望的价格"
-                                }
-                            },
-                        "required": ["location"]
-                        },
-                    "responses": {
-                        "type": "object",
-                        "properties": {
-                            "id": {
-                                "type": "string",
-                                "description": "商品id"
-                                },
-                            "price": {
-                                "type": "int",
-                                "description": "商品价格"
-                                },
-                            "food": {
-                                "type": "string",
-                                "description": "商品名称"
-                                },
-                            },
-                        },
-                 },
-                 {
-                        "name": "delivery_order",
-                        "description": "外卖下单",
-                        "parameters": {
-                            "type": "object",
-                            "properties": {
-                                "id": {
-                                    "type": "string",
-                                    "description": "商品id"
-                                    },
-                                "food": {
-                                    "type": "string",
-                                    "description": "商品名称"
-                                    },
-                                },
-                            "required": ["id"]
-                            },
-                        "responses": {
-                            "type": "object",
-                            "properties": {
-                                "result": {
-                                    "type": "string",
-                                    "description": "是否下单成功"
-                                    },
-                                }
-                            },
-                   }
-                 ]
-            )
-
-
-    st.write(response)
-
-
-
-
-
-
-
-
-
-
-
 
 
 # chunk the data
@@ -288,12 +195,95 @@ if __name__ == "__main__":
         else:
             st.write("There is nothing to be summarised")
     
+import qianfan
+import re
 
-chat_comp = qianfan.ChatCompletion()
-prompt = "东升街道附近20元的午餐有哪些推荐？###黄焖鸡不错，就买这个19号的黄焖鸡了###"
+
+def eb_call(prompt, round):
+    print(prompt)
+    print('-' * 20,' Output ', '-'*20,"\n")
+
+    response = chat_comp.do(
+            model="ERNIE-Bot", 
+            messages=[{
+                "role": "user",
+                "content": prompt
+                }],
+            temperature=0.000000001,
+            functions=[
+                {
+                    "name": "delivery_inquiry",
+                    "description": "查询商品",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "地址信息，包括街道、门牌号、城市、省份等信息"
+                                },
+                            "expect_price": {
+                                "type": "int",
+                                "description": "期望的价格"
+                                }
+                            },
+                        "required": ["location"]
+                        },
+                    "responses": {
+                        "type": "object",
+                        "properties": {
+                            "id": {
+                                "type": "string",
+                                "description": "商品id"
+                                },
+                            "price": {
+                                "type": "int",
+                                "description": "商品价格"
+                                },
+                            "food": {
+                                "type": "string",
+                                "description": "商品名称"
+                                },
+                            },
+                        },
+                 },
+                 {
+                        "name": "delivery_order",
+                        "description": "外卖下单",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "id": {
+                                    "type": "string",
+                                    "description": "商品id"
+                                    },
+                                "food": {
+                                    "type": "string",
+                                    "description": "商品名称"
+                                    },
+                                },
+                            "required": ["id"]
+                            },
+                        "responses": {
+                            "type": "object",
+                            "properties": {
+                                "result": {
+                                    "type": "string",
+                                    "description": "是否下单成功"
+                                    },
+                                }
+                            },
+                   }
+                 ]
+            )
+
+
+    st.write(response)
+    return response
+
+chat_comp = qianfan.ChatCompletion(ak="LrQvpiE6f4npsUwEvPL9vEWF", sk="CHTwBMVM0DlwyoGTLGEyRviBdctgOv4G")
+prompt = "百度大厦-北京海淀区上地十街10号的附近，午餐有哪些推荐？###能不能帮我点一个20元以内的？###"
 prompt_list = re.split(r"###", prompt)
 
-for pp in prompt_list:
-    eb_call(pp, round)
-    st.write('=' * 60,"\n")
-    
+for questions in prompt_list:
+    response = eb_call(questions, round)
+    st.write(response['result'])
